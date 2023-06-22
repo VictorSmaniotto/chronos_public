ac3b4475c46c72486cdd1f556ec40ff1b719aaf8
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-06-21 21:24:48 -0300

    Configurei a api com endpoints de login, logout e cadastro de usuarios

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+
+            $usuario = Auth::user(); //Retorna as informações do usuário autenticado
+
+            return response()->json([
+                'status' => true,
+                'nome' => $usuario->nome,
+                'email' => $usuario->email,
+                'foto' => $usuario->foto,
+                'token' => $usuario->createToken('AppChronos')->plainTextToken
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'mensagem' => "Usuário não autorizado"
+            ], 401);
+        };
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete(); //deleta apenas o token que está fazendo logout
+        return response()->json([
+            'mensagem' => "Logout realizado com sucesso"
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Api/UsuarioController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+
+class UsuarioController extends Controller
+{
+    public function usuario(Request $request)
+    {
+        $usuario = $request->user();
+
+        $dadosUsuario = [
+            'nome' => $usuario->nome,
+            'email' => $usuario->email,
+            'foto' => url($usuario->foto)
+        ];
+
+        return response()->json($dadosUsuario);
+    }
+
+    public function registrar(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+
+        ]);
+
+
+        $usuario = new User();
+        $usuario->nome = $request->nome;
+        $usuario->email = $request->email;
+        $usuario->password = bcrypt($request->password);
+
+
+        if ($request->hasFile('foto')) {
+            $foto = $request->file('foto');
+            $fotoNome = $foto->hashName();
+            $fotoPath = $foto->storeAs('public/usuarios/', $fotoNome);
+            $usuario->foto = Storage::url($fotoPath);
+        }
+
+
+        $usuario->save();
+
+        return response()->json([
+            'mensagem' => 'Usuário cadastrado com sucesso',
+            'Dados' => $usuario
+        ]);
+    }
+}
