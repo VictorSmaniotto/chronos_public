a99371fb1dd7e7e1e4f403db30df528eb1cf6cf8
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-06-23 21:55:50 -0300

    Coloquei os tempos na publicação

--- resources/views/site/visualizar.blade.php
+++ resources/views/site/visualizar.blade.php
@@ -6,7 +6,18 @@
     <header class="main">
       <h1>{{ $projeto->titulo }}</h1>
     </header>
-
+    <span class="text-muted mb-1 me-1">
+        <i class="fa-regular fa-calendar-days"></i>
+        Publicado em {{ $projeto->created_at->format('d/m/Y') }}
+    </span>
+    <span class="text-muted mb-1 me-1">
+        <i class="fa-regular fa-clock"></i>
+        Atualizado {{ $data->diffForHumans() }}
+    </span>
+    <span class="text-muted mb-1">
+        <i class="fa-solid fa-bookmark"></i>
+        Categoria: {{ $projeto->categoria->nome }}
+    </span>
     <span class="image main"
       ><img src="{{ $projeto->capa }}" alt=""
     /></span>
