a3f57abc72d18e70d495b0318b0458197cef274f
lais <laismidori51@gmail.com>
2023-06-23 21:58:02 -0300

    mudança de rota da imagem

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -133,7 +133,7 @@
 <body class="d-flex flex-column min-vh-100">
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6"
-           href="#"><img src="/img/logo-branca.png" class="img-fluid" width="150"></a>
+           href="{{route('site.index')}}"><img src="{{assets('img/logo-branca.png')}}" class="img-fluid" width="150"></a>
         <button class="navbar-toggler position-absolute d-md-none collapsed"
                 type="button"
                 data-bs-toggle="collapse"
