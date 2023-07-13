c24f62b2c24d11fa8067506e88d9b4588e9bce9e
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-07-13 19:17:50 -0300

    sobre nois

---
+++ resources/views/site/sobrenos.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.site')
+
+@section('conteudo')
+
+{{-- <section id="banner">
+    <div class="content">
+      <header>
+        <h1 class="fs-1">
+          Bem vindo(a) ao Chronos
+        </h1>
+        <p>Acompanhe os projetos desenvolvidos por sua comunidade</p>
+      </header>
+      <p>
+        Nós apresentamos o Chronos, uma plataforma revolucionária que vai transformar a forma como gerenciamos e publicamos nossos projetos escolares. Com recursos abrangentes e uma interface intuitiva, o Chronos oferece uma experiência completa, promovendo a colaboração, o compartilhamento de conhecimento e o nosso crescimento acadêmico.
+      </p>
+      <p>
+        Uma das grandes vantagens do Chronos é a facilidade de centralizar todos os aspectos relacionados aos nossos projetos em um único lugar. Podemos criar e gerenciar nossos projetos de forma organizada, inserindo informações detalhadas como título, descrição, objetivo e os membros do nosso grupo. Isso nos permite colaborar de maneira mais eficiente e alcançar melhores resultados juntos.
+      </p>
+      <p>
+        Além disso, o Chronos nos oferece um espaço dedicado para a publicação dos nossos projetos. Essa plataforma pública permite que outros estudantes, professores e até mesmo a nossa comunidade escolar tenham acesso aos nossos projetos, possibilitando a troca de ideias, inspiração e aprendizado mútuo. Essa exposição dos nossos projetos também nos incentiva a nos esforçarmos e aprimorarmos nossas habilidades, já que temos a oportunidade de mostrar nosso trabalho para um público maior.
+      </p>
+      <p>
+        O Chronos também nos proporciona recursos interativos para nos engajarmos ainda mais. O mural de ajuda é um espaço onde podemos tirar dúvidas, compartilhar informações adicionais e participar de pesquisas relacionadas aos nossos projetos. Isso cria uma comunidade de aprendizado ativa, onde podemos obter suporte, trocar conhecimentos e explorar diferentes perspectivas juntos.
+      </p>
+      <p>
+        A plataforma também possui um chat integrado, que facilita a comunicação entre os membros do nosso grupo. Isso nos ajuda a colaborar em tempo real, compartilhar recursos e resolver problemas juntos. Além disso, os administradores têm a capacidade de moderar os chats, garantindo um ambiente seguro e respeitoso para todos nós.
+      </p>
+      <p>
+        Com uma interface moderna e responsiva, o Chronos proporciona uma experiência agradável em diferentes dispositivos, como computadores, tablets e smartphones. Isso nos permite acessar e interagir com a plataforma de forma conveniente, a qualquer momento e em qualquer lugar.
+      </p>
+      <p>
+        Em suma, o Chronos é a nossa solução completa para gerenciar projetos, colaborar com nossos colegas, compartilhar nossos trabalhos e nos conectar com nossa comunidade escolar. Com seus recursos robustos e intuitivos, o Chronos impulsiona nosso aprendizado colaborativo, estimula nosso desenvolvimento de habilidades e fortalece nossos laços na escola. É a ferramenta essencial para o nosso sucesso educacional e nosso crescimento acadêmico.
+      </p>
+
+    </div>
+    <span class="image object">
+      <img src="{{ asset('img/exports/projetos.jpg') }}" alt="" />
+    </span>
+  </section> --}}
+
+  <section>
+    <header class="major">
+      <h2>Bem vindo(a) ao Chronos</h2>
+    </header>
+    <div class="container">
+
+        <article>
+
+
+            <p>
+                Nós apresentamos o Chronos, uma plataforma revolucionária que vai transformar a forma como gerenciamos e publicamos nossos projetos escolares. Com recursos abrangentes e uma interface intuitiva, o Chronos oferece uma experiência completa, promovendo a colaboração, o compartilhamento de conhecimento e o nosso crescimento acadêmico.
+              </p>
+              <p>
+                Uma das grandes vantagens do Chronos é a facilidade de centralizar todos os aspectos relacionados aos nossos projetos em um único lugar. Podemos criar e gerenciar nossos projetos de forma organizada, inserindo informações detalhadas como título, descrição, objetivo e os membros do nosso grupo. Isso nos permite colaborar de maneira mais eficiente e alcançar melhores resultados juntos.
+              </p>
+              <p>
+                Além disso, o Chronos nos oferece um espaço dedicado para a publicação dos nossos projetos. Essa plataforma pública permite que outros estudantes, professores e até mesmo a nossa comunidade escolar tenham acesso aos nossos projetos, possibilitando a troca de ideias, inspiração e aprendizado mútuo. Essa exposição dos nossos projetos também nos incentiva a nos esforçarmos e aprimorarmos nossas habilidades, já que temos a oportunidade de mostrar nosso trabalho para um público maior.
+              </p>
+              <p>
+                O Chronos também nos proporciona recursos interativos para nos engajarmos ainda mais. O mural de ajuda é um espaço onde podemos tirar dúvidas, compartilhar informações adicionais e participar de pesquisas relacionadas aos nossos projetos. Isso cria uma comunidade de aprendizado ativa, onde podemos obter suporte, trocar conhecimentos e explorar diferentes perspectivas juntos.
+              </p>
+              <p>
+                A plataforma também possui um chat integrado, que facilita a comunicação entre os membros do nosso grupo. Isso nos ajuda a colaborar em tempo real, compartilhar recursos e resolver problemas juntos. Além disso, os administradores têm a capacidade de moderar os chats, garantindo um ambiente seguro e respeitoso para todos nós.
+              </p>
+              <p>
+                Com uma interface moderna e responsiva, o Chronos proporciona uma experiência agradável em diferentes dispositivos, como computadores, tablets e smartphones. Isso nos permite acessar e interagir com a plataforma de forma conveniente, a qualquer momento e em qualquer lugar.
+              </p>
+              <p>
+                Em suma, o Chronos é a nossa solução completa para gerenciar projetos, colaborar com nossos colegas, compartilhar nossos trabalhos e nos conectar com nossa comunidade escolar. Com seus recursos robustos e intuitivos, o Chronos impulsiona nosso aprendizado colaborativo, estimula nosso desenvolvimento de habilidades e fortalece nossos laços na escola. É a ferramenta essencial para o nosso sucesso educacional e nosso crescimento acadêmico.
+              </p>
+
+        </article>
+
+
+    </div>
+  </section>
+
+
+@endsection
+
+
